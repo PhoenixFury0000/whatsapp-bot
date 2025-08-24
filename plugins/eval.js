233e702bb8e2853bc1ae5fc560c6676572415471
naxor <naxordeve@gmail.com>
2025-08-24 10:46:16 +0200

    mods

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,18 +1,18 @@
-const { mods } = require('../config');
-const {Module} = require('../lib/plugins');
-const util = require('util');
-
+const { mods } = require('../config');  
+const {Module} = require('../lib/plugins');  
+const util = require('util');  
+  
 Module({
   on: 'text'
 })(async (message) => {
   if (!message.body.startsWith('>')) return;
-  if (!(message.fromMe || mods.includes(message.sender))) return;
+  let sender = message.sender.split('@')[0]; 
+  if (!(message.fromMe || mods.includes(sender))) return;
   let code = message.body.slice(1).trim();
-  try {
-    let result = await eval(`(async () => { ${code} })()`);
-    if (typeof result !== 'string') result = require('util').inspect(result);
-    await message.send(result );
+  try {let result = await eval(`(async () => { ${code} })()`);
+  if (typeof result !== 'string') result = require('util').inspect(result);
+  await message.send(result);
   } catch (e) {
-    await message.send(e.message);
+  await message.send(e.message);
   }
 });
