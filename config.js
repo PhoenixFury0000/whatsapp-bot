713c6510b03ec58c2d88d6ce9b822385a839a42d
𝚸𝚮𝚯𝚵𝚴𝚰𝚾 <piyushguptaofficial07042009@gmail.com>
2025-08-29 00:09:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const isTrue = (x) => x === 'true' || x === true;
 
 module.exports = {
     prefix: process.env.PREFIX || '.',
-    owner: process.env.OWNER_NUMBER || '',
+    owner: process.env.OWNER_NUMBER || '917973456275',
     sudo: process.env.SUDO || '',
-    packname: process.env.PACKNAME || 'ɠαɾϝιҽʅɗ',
-    author: process.env.AUTHOR || 'ɳαxσɾ',
+    packname: process.env.PACKNAME || 'phoenix',
+    author: process.env.AUTHOR || 'PHOENIX',
     SESSION_ID: process.env.SESSION_ID || '',
     THEME: process.env.THEME || '', //Garfield
     maxFileSize: parseInt(process.env.MAX_FILE_SIZE) || 100 * 1024 * 1024,
